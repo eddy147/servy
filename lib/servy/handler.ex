3b54fe9598c570d7494fea6d886836bde0c3e1bb
Eddy de Boer <eddy.de.boer@medicore.nl>
2020-12-04 10:35:30 +0100

    Formatted

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,5 +1,4 @@
 defmodule Servy.Handler do
-
   @moduledoc "Handles HTTP requests."
 
   alias Servy.Conv
@@ -29,35 +28,35 @@ defmodule Servy.Handler do
     Servy.PledgeController.index(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
+  def route(%Conv{method: "GET", path: "/sensors"} = conv) do
     sensor_data = Servy.SensorServer.get_sensor_data()
 
-    %{ conv | status: 200, resp_body: inspect sensor_data }
+    %{conv | status: 200, resp_body: inspect(sensor_data)}
   end
 
-  def route(%Conv{ method: "GET", path: "/kaboom" }) do
+  def route(%Conv{method: "GET", path: "/kaboom"}) do
     raise "Kaboom!"
   end
 
-  def route(%Conv{ method: "GET", path: "/hibernate/" <> time } = conv) do
-    time |> String.to_integer |> :timer.sleep
+  def route(%Conv{method: "GET", path: "/hibernate/" <> time} = conv) do
+    time |> String.to_integer() |> :timer.sleep()
 
-    %{ conv | status: 200, resp_body: "Awake!" }
+    %{conv | status: 200, resp_body: "Awake!"}
   end
 
-  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%Conv{ method: "GET", path: "/api/bears" } = conv) do
+  def route(%Conv{method: "GET", path: "/api/bears"} = conv) do
     Servy.Api.BearController.index(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     BearController.index(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     params = Map.put(conv.params, "id", id)
     BearController.show(conv, params)
   end
@@ -67,26 +66,26 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/about"} = conv) do
-      @pages_path
-      |> Path.join("about.html")
-      |> File.read
-      |> handle_file(conv)
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
-  def route(%Conv{ path: path } = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!"}
+  def route(%Conv{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content }
+    %{conv | status: 200, resp_body: content}
   end
 
   def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!" }
+    %{conv | status: 404, resp_body: "File not found!"}
   end
 
   def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
   end
 
   def format_response(%Conv{} = conv) do
@@ -98,5 +97,4 @@ defmodule Servy.Handler do
     #{conv.resp_body}
     """
   end
-
 end
